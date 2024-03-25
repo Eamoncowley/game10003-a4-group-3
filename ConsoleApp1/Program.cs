f8904dde593e54c4e0963d3874b6f3b63e7a5e0d
LostPhantom18 <Jonah_Bron@Outlook.com>
2024-03-25 04:41:46 -0400

    Install raylib

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace ConsoleApp1
+﻿using Raylib_cs;
+using System.Numerics;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
+        // Console.WriteLine("Setting up the file");
+
+        const string title = "Basic Raylib Demo";
+        const int width = 800;
+        const int height = 800;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Setting up the file");
+            Raylib.InitWindow(width, height, title);
+            Raylib.SetTargetFPS(60);
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.White);
+                Vector2 position = Raylib.GetMousePosition();
+                Raylib.DrawCircleV(position, 50, Color.Black);
+                Raylib.EndDrawing();
+            }
+            Raylib.CloseWindow();
         }
     }
 }
